cc4461925d2943354f2a80b4dad824daff5edcd6
ksantora8 <kathsantora@gmail.com>
2021-02-26 17:28:28 -0500

    Created pokemon list array

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,62 @@
-alert('Hello world');
-let favoriteFood = 'oysters';
-document.write(favoriteFood);
+let pokemonList = [
+ {
+   name = 'Bulbasaur',
+   type = [ 'grass' , 'poison' ]
+   attack = 49 ,
+   defense = 49 ,
+ } ,
+ {
+   name = 'Charmander',
+   type = [ 'fire' ]
+   attack = 52 ,
+   defense = 43 ,
+ } ,
+ {
+   name = 'Squirtle',
+   type = [ 'water' ]
+   attack = 48 ,
+   defense = 45 ,
+ } ,
+ {
+   name = 'Caterpie',
+   type = [ 'bug' ]
+   attack = 30 ,
+   defense = 35 ,
+ } ,
+ {
+   name = 'Weedle',
+   type = [ 'bug' , 'poison' ]
+   attack = 35 ,
+   defense = 30 ,
+ } ,
+ {
+   name = 'Pidgey',
+   type = [ 'normal' , 'flying' ]
+   attack = 45 ,
+   defense = 40 ,
+ } ,
+ {
+   name = 'Rattata',
+   type = [ 'normal' ]
+   attack = 56 ,
+   defense = 35 ,
+ } ,
+ {
+   name = 'Spearow',
+   type = [ 'normal' , 'flying' ]
+   attack = 60 ,
+   defense = 30 ,
+ } ,
+ {
+   name = 'Ekans',
+   type = [ 'poison' ]
+   attack = 60 ,
+   defense = 44 ,
+ } ,
+ {
+   name = 'Pikachu',
+   type = [ 'electric' ]
+   attack = 55 ,
+   defense = 40 ,
+ }
+];
