2e7a0aa0f124c0278cac3d502e10af21fc5f4f58
ksantora8 <kathsantora@gmail.com>
2021-03-11 23:15:04 -0500

    added API, polyfills, changed getAll to getArray, changed addPokemon to addArray, corrected addListener, added loadList and loadDetails

--- js/scripts.js
+++ js/scripts.js
@@ -1,88 +1,21 @@
-let pokemonRepository = (function(){
-  let pokemonList = [
-        {
-          name : 'Bulbasaur',
-          type : [ 'grass' , 'poison' ],
-          height : 0.7 ,
-          attack : 49 ,
-          defense : 49 ,
-        } ,
-        {
-          name : 'Charmander',
-          type : [ 'fire' ],
-          height : 0.6 ,
-          attack : 52 ,
-          defense : 43 ,
-        } ,
-        {
-          name : 'Squirtle',
-          type : [ 'water' ],
-          height : 0.5 ,
-          attack : 48 ,
-          defense : 45 ,
-        } ,
-        {
-          name : 'Caterpie',
-          type : [ 'bug' ],
-          height : 0.3 ,
-          attack : 30 ,
-          defense : 35 ,
-        } ,
-        {
-          name : 'Weedle',
-          type : [ 'bug' , 'poison' ],
-          height : 0.3 ,
-          attack : 35 ,
-          defense : 30 ,
-        } ,
-        {
-          name : 'Pidgey',
-          type : [ 'normal' , 'flying' ],
-          height : 0.3 ,
-          attack : 45 ,
-          defense : 40 ,
-        } ,
-        {
-          name : 'Rattata',
-          type : [ 'normal' ],
-          height : 0.3 ,
-          attack : 56 ,
-          defense : 35 ,
-        } ,
-        {
-          name : 'Spearow',
-          type : [ 'normal' , 'flying' ],
-          height : 0.3 ,
-          attack : 60 ,
-          defense : 30 ,
-        } ,
-        {
-          name : 'Ekans',
-          type : [ 'poison' ],
-          height : 2 ,
-          attack : 60 ,
-          defense : 44 ,
-        } ,
-        {
-          name : 'Pikachu',
-          type : [ 'electric' ],
-          height : 0.4 ,
-          attack : 55 ,
-          defense : 40 ,
-        }
-        ];
+let pokemonRepository = (() => {
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
-  function getAll(){
+  function getArray(){
       return pokemonList;
     }
 
-  function addPokemon(item){
-    if(typeof(item)=='object'){
-      let newPokeProperty = Object.keys(item);
-      if (newPokeProperty.includes('name') && newPokeProperty.includes('height') && newPokeProperty.includes('type') && newPokeProperty.includes('attack') && newPokeProperty.includes('defense'))
-      {pokemonList.push(item)}else{
-      alert('Missing properties!');
+  function addToArray(pokemon){
+    if(typeof(pokemon)=='object'){
+      let newPokeProperty = Object.keys(pokemon);
+      if (newPokeProperty.includes('name') && newPokeProperty.includes('detailsUrl')){
+        pokemonList.push(pokemon)
+      }else{
+        alert('Incorrect Keys');
       }
+    } else {
+      alert ('All inputs must be objects.');
     }
   }
 
@@ -92,34 +25,59 @@ let pokemonRepository = (function(){
     let listButton = document.createElement('button');
     listButton.innerText = pokemon.name;
     listButton.classList.add('custom-button');
-    list.appendChild(listButton);
+    listItem.appendChild(listButton);
     list.appendChild(listItem);
-    addListener(listButton, pokemon);
+    addListener(listButton, showDetails, pokemon);
   }
 
-  function addListener(listButton, pokemon){
-      listButton.addEventListener('click', () => showDetails(pokemon));
+  function addListener(element, method , object){
+      element.addEventListener('click', () => method(object));
   }
 
   function showDetails(pokemon){
-      console.log(pokemon)
+      loadDetails(pokemon).then(function () {
+        console.log(pokemon);
+      });
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        addToArray(pokemon);
+      });
+    }).catch(function (err) {
+      console.error(err);
+    })
+  }
+
+  //go back in and correct syntax to match rest of code
+  function loadDetails(pokemon){
+    let url = pokemon.detailsUrl;
+    return fetch(url)
+          .then(response => response.json())
+          .then(details => {
+             pokemon.imageUrl = details.sprites.front_default;
+             pokemon.height = details.height;
+             pokemon.types = details.types;
+          })
+          .catch(err => console.log(err))
+}
+
   return{
-    getAll: getAll,
-    addPokemon: addPokemon,
+    getArray: getArray,
+    addToArray: addToArray,
     addListItem: addListItem,
+    loadList: loadList,
+    loadDetails : loadDetails,
   }
 })();
 
-//function display(pokemon) {
-    //if (pokemon.height > 1){
-     // document.write (pokemon.name + "  (Height: " + pokemon.height + ") - Wow! That's tall! " + "<br> <br>");
-    //}else{
-     // document.write (pokemon.name + "  (Height: " + pokemon.height + ") <br> <br>");
-    //}
- // }
-
-//pokemonRepository.addPokemon({ name: 'Sandshrew', type: ['ground'], height: 0.2, attack: 75, defense:85, });
-
-pokemonRepository.getAll().forEach(pokemon => pokemonRepository.addListItem(pokemon));
+pokemonRepository.loadList().then(function() {
+  pokemonRepository.getArray().forEach(pokemon => pokemonRepository.addListItem(pokemon));
+});
